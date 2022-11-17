e1b096231b2c04e996b484a8ed0a36344b98308c
Greg Copeland <greg.copeland@me.com>
2022-11-17 09:36:40 -0500

    Initial commit

---
+++ MeshInstance3D.gd
@@ -0,0 +1,39 @@
+extends MeshInstance3D
+@tool
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mesh_data = []
+	mesh_data.resize(ArrayMesh.ARRAY_MAX)
+	mesh_data[ArrayMesh.ARRAY_VERTEX] = PackedVector3Array(
+		[
+			Vector3(0,0,0),
+			Vector3(1,0,0),
+			Vector3(1,1,0),
+			Vector3(0,1,0)
+		]
+	)
+	mesh_data[ArrayMesh.ARRAY_INDEX] = PackedInt32Array(
+		[
+			0,1,2,
+			0,2,3
+		]
+	)
+	mesh_data[ArrayMesh.ARRAY_NORMAL] = PackedVector3Array(
+		[
+			Vector3(0,0,-1),
+			Vector3(0,0,-1),
+			Vector3(0,0,-1),
+			Vector3(0,0,-1)									
+		]
+	)
+	mesh_data[ArrayMesh.ARRAY_TEX_UV] = PackedVector2Array(
+		[
+			Vector2(1,1),
+			Vector2(0,1),
+			Vector2(0,0),
+			Vector2(1,0)									
+		]
+	)
+	mesh = ArrayMesh.new()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
